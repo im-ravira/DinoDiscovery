946fb937ee09e44715c2a314066114a0bf1417d8
im-ravira <imravira@gmail.com>
2024-09-15 15:47:34 +0530

    initializing the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-lime-300 text-6xl text-center mt-10'>Initial Setup</h1>
+    </>
+  )
+}
+
+export default App
