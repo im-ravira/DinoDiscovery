474d51c4e213091d85050effd3146d51fbdc7e30
im-ravira <imravira@gmail.com>
2024-09-18 21:51:05 +0530

    feat: created FunFacts page

---
+++ src/components/FunFacts1.component.jsx
@@ -0,0 +1,19 @@
+import dinofact from "../assets/dinofact-1.png";
+
+const FunFacts1 = ({ facts }) => {
+    return (
+        <section className="w-[80vw] flex flex-col items-center p-16 gap-14 mt-32">
+            <div className="flex w-full justify-between gap-16 mt-8">
+                <div className="w-[25vw] shadow-black shadow-xl translate-y-6 -rotate-6 opacity-0" id="img">
+                    <img src={dinofact} alt="" />
+                </div>
+                <div className="flex w-[45vw] flex-col justify-self-end items-center justify-center text-center">
+                    <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24">{facts}</h1>
+                    <a href="https://www.nhm.ac.uk/discover/dino-directory/" target="blank" className="underline text-white links opacity-0 -translate-x-24">Know More</a>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default FunFacts1;
---
+++ src/pages/FunFacts.jsx
@@ -0,0 +1,51 @@
+import Navbar from "../components/Navbar";
+import FunFacts1 from "../components/FunFacts1.component";
+import FunFacts2 from "../components/FunFacts2.component";
+import { gsap } from "gsap";
+import { useRef, useEffect } from "react";
+
+const FunFacts = () => {
+    const main = useRef(null);
+    const q = gsap.utils.selector(main);
+
+    useEffect(() => {
+        gsap.to(q('#img'), {
+            x: 0,
+            opacity: 1,
+            stagger: .1,
+            duration: 1
+        });
+
+        gsap.to(q('.facts'), {
+            x: 0,
+            opacity: 1,
+            delay: .3,
+            duration: 1
+        });
+
+        gsap.to(q('.links'), {
+            x: 0,
+            opacity: 1,
+            delay: .4,
+            duration: 1
+        });
+    }, []);
+
+    return (
+        <main className="w-full flex-col flex bg-fixed bg-secondary bg-no-repeat bg-cover items-center justify-center min-h-screen text-white relative overflow-hidden" ref={main}>
+            <Navbar />
+            <FunFacts1 facts="Dinosaurs lived millions of years ago." />
+            <FunFacts2 facts="Some dinosaurs were as small as chickens!" />
+            <FunFacts1 facts="The T. Rex had the strongest bite of any land animal." />
+            <FunFacts2 facts="Dinosaurs laid eggs like birds do today." />
+            <FunFacts1 facts="Some dinosaurs had feathers." />
+            <FunFacts2 facts="Argentinosaurus was as heavy as 15 elephants!" />
+            <FunFacts1 facts="Birds are the closest living relatives to dinosaurs." />
+            <FunFacts2 facts="Velociraptors were only the size of turkeys." />
+            <FunFacts1 facts="Stegosaurus had a brain the size of a walnut." />
+            <FunFacts2 facts="Dinosaurs once lived on every continent, including Antarctica!" />
+        </main>
+    );
+}
+
+export default FunFacts;
